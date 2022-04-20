f8025f2294d45e10db71ae18a957edd7416357e1
DESKTOP-DNP38AM\coffe <coffee_soul@navercom>
2022-04-20 17:56:45 +0900

    2022 4/20 3장완료

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
+
+}
---
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -0,0 +1,36 @@
+package com.jojoldu.book.springboot.service;
+
+import com.jojoldu.book.springboot.domain.posts.*;
+import com.jojoldu.book.springboot.web.dto.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
